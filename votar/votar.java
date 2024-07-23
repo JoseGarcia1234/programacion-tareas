4b6994a7f5ec0705cc49e93ebada037b0f553290
Jose Garcia <nazag9664@gmail.com>
2024-07-23 14:15:03 -0600

    Agregado de votar

---
+++ votar/votar.java
@@ -0,0 +1,32 @@
+import java.util.Scanner; 
+
+public class votar { 
+
+public static void main(String[] args) { 
+
+        Scanner Entrada = new Scanner(System.in); 
+
+        int edad=0; 
+
+        System.out.println("NOTA PARA VOTAR LA EDAD MINIMA ES DE 18 AÑOS"); 
+
+        System.out.println("-----------------------------------------------"); 
+
+        System.out.println("DAME TU EDAD:"); 
+
+        edad= Entrada.nextInt(); 
+
+        if(edad>=18){ 
+
+            System.err.println("PUEDES VOTAR "); 
+
+        }else{ 
+
+            System.out.println("NO PUEDES VOTAR"); 
+
+        } 
+        Entrada.close();
+
+    } 
+
+} 
