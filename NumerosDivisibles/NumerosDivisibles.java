1e3ae4fe23cce9796fd5ec998c433028590e8d84
Jose Garcia <nazag9664@gmail.com>
2024-07-24 22:27:48 -0600

    Agregado divi

--- NumerosDivisibles/NumerosDivisibles.java
+++ NumerosDivisibles/NumerosDivisibles.java
@@ -1,5 +1,4 @@
 public class NumerosDivisibles {
-
     public static void main(String[] args){
         System.out.println("Numeros divisbles por 3 y 5 entre 1 y 100: ");
         for (int i = 1; i <= 100; i++){
